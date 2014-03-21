6e6f78d49adb3364fc68422e85be946b3c29302d
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-03-21 16:40:15 -0700

    Modified schema to be what we think we need

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,8 +3,27 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
- /* Describe your table here.*/
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user number(10) NOT NULL, 
+  created_at date NOT NULL,
+  text varchar(160),
+  room number(3) NOT NULL,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE room (
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE user (
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name varchar(12) NOT NULL,
+  room number(3) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 
 /* You can also create more tables, if you need them... */
 
